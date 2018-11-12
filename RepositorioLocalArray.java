264e587d4fe7ad2f931381b9fe5bed4de174e8ec
ecf2 <43381805+ecf2@users.noreply.github.com>
2018-11-12 11:44:56 -0200

    Add files via upload

---
+++ RepositorioLocalArray.java
@@ -0,0 +1,17 @@
+package Local;
+
+	public class RepositorioLocalArray implements repositorioLocais {
+	    private Local[] arrayLocal = new Local[200];
+	    private int contador = 0;
+
+	    
+	    //Verifica se o array nao esta cheio e insere a rota  
+	    public void inserir(Local local) {
+	           
+	        if (this.contador < this.arrayLocal.length) {
+	            this.arrayLocal[contador] = local;
+	            this.contador++;
+             }
+	    
+	     }
+ }
---
+++ repositorioLocais.java
@@ -0,0 +1,18 @@
+package Local;
+
+public interface repositorioLocais {
+	
+	   public void inserir();
+	            
+	    public void atualizar();
+	           
+
+	    public void remover();
+	           
+
+	    public boolean procurar();
+	            
+
+	    public boolean existe();
+	}
+
